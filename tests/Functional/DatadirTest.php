15e9bc6699c239851919458642d7d0b62cb1d643
David Střítežský <stritezskyd@gmail.com>
2018-09-12 13:28:23 +0200

    Add first DataDir test

---
+++ tests/Functional/DatadirTest.php
@@ -0,0 +1,70 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Keboola\DbExtractor\Tests\Functional;
+
+use Keboola\DatadirTests\AbstractDatadirTestCase;
+use Keboola\DatadirTests\DatadirTestSpecification;
+use Keboola\DbExtractor\Test\DataLoader;
+
+class DatadirTest extends AbstractDatadirTestCase
+{
+    protected function getScript(): string
+    {
+        return $this->getTestFileDir() . '/run.php';
+    }
+
+    private function getConfig(string $testDirectory): array
+    {
+        $configuration = json_decode((string) file_get_contents($testDirectory . '/config.json'), true);
+        $configuration['parameters']['data_dir'] = $testDirectory;
+        $configuration['parameters']['extractor_class'] = 'common';
+        return$configuration;
+    }
+
+    public function testActionTestConnection(): void
+    {
+        $testDirectory = __DIR__ . '/action-test-connection';
+
+        $configuration = $this->getConfig($testDirectory);
+
+        $credentials = $configuration['parameters']['db'];
+        $dataLoader = new DataLoader(
+            $credentials['host'],
+            $credentials['port'],
+            $credentials['database'],
+            $credentials['user'],
+            $credentials['#password']
+        );
+        $dataLoader->getPdo()->exec(sprintf(
+            "DROP DATABASE IF EXISTS `%s`",
+            $credentials['database']
+        ));
+        $dataLoader->getPdo()->exec(sprintf(
+            "CREATE DATABASE `%s`
+            DEFAULT CHARACTER SET utf8
+            DEFAULT COLLATE utf8_general_ci",
+            $credentials['database']
+        ));
+
+        $response = ['status' => 'success'];
+
+        $specification = new DatadirTestSpecification(
+            $testDirectory . '/source/data',
+            0,
+            json_encode($response, JSON_PRETTY_PRINT),
+            null,
+            $testDirectory . '/expected/data/out'
+        );
+        $tempDatadir = $this->getTempDatadir($specification);
+
+        $configuration['parameters']['db'] = $credentials;
+        file_put_contents(
+            $tempDatadir->getTmpFolder() . '/config.json',
+            json_encode($configuration, JSON_PRETTY_PRINT)
+        );
+        $process = $this->runScript($tempDatadir->getTmpFolder());
+        $this->assertMatchesSpecification($specification, $process, $tempDatadir->getTmpFolder());
+    }
+}
---
+++ tests/Functional/run.php
@@ -0,0 +1,34 @@
+<?php
+
+require_once __DIR__ . '/../../vendor/autoload.php';
+
+use Keboola\DbExtractor\Application;
+use Keboola\DbExtractor\Exception\UserException;
+use Keboola\DbExtractor\Logger;
+
+$datadirPath = rtrim(getenv('KBC_DATADIR'), '/');
+
+$config = json_decode((string) file_get_contents($datadirPath . '/config.json'), true);
+$logger = new Logger('datadir-tests');
+
+$app = new Application($config, $logger);
+
+try {
+    echo json_encode($app->run(), JSON_PRETTY_PRINT);
+    exit(0);
+} catch (UserException $e) {
+    $logger->error($e->getMessage());
+    exit(1);
+} catch (\Throwable $e) {
+    $logger->critical(
+        get_class($e) . ':' . $e->getMessage(),
+        [
+            'errFile' => $e->getFile(),
+            'errLine' => $e->getLine(),
+            'errCode' => $e->getCode(),
+            'errTrace' => $e->getTraceAsString(),
+            'errPrevious' => $e->getPrevious() ? get_class($e->getPrevious()) : '',
+        ]
+    );
+    exit(2);
+}
