d053ebe9647fb85ba557c2d85b8c5bf65746d871
pivnicek <marc@keboola.com>
2018-09-21 16:30:41 +0200

    add retryProxy tests for sql codes

---
+++ tests/RetryProxyTest.php
@@ -0,0 +1,75 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Keboola\DbExtractor\Tests;
+
+use Keboola\DbExtractor\Logger;
+use Keboola\DbExtractor\Retry\RetryProxy;
+use Keboola\DbExtractor\Test\DataLoader;
+use Keboola\DbExtractor\Test\ExtractorTest;
+use Monolog\Handler\TestHandler;
+
+class RetryProxyTest extends ExtractorTest
+{
+    public const DRIVER = 'common';
+    /**
+     * @var  \PDO
+     */
+    private $db;
+
+    public function setUp()
+    {
+        $dataLoader = new DataLoader(
+            $this->getEnv(self::DRIVER, 'DB_HOST'),
+            $this->getEnv(self::DRIVER, 'DB_PORT'),
+            $this->getEnv(self::DRIVER, 'DB_DATABASE'),
+            $this->getEnv(self::DRIVER, 'DB_USER'),
+            $this->getEnv(self::DRIVER, 'DB_PASSWORD')
+        );
+        $this->db = $dataLoader->getPdo();
+        $this->db->setAttribute(\PDO::ATTR_ERRMODE, \PDO::ERRMODE_EXCEPTION);
+    }
+
+    /**
+     * @param $sqlCodes
+     * @dataProvider ignorableSqlCodesProvider
+     */
+    public function testSkipRetryFor42(array $sqlCodes): void
+    {
+        $logger = new Logger('test-retry-proxy-logger');
+        $testHandler = new TestHandler();
+        $logger->pushHandler($testHandler);
+
+        $retryProxy = new RetryProxy(
+            $logger,
+            RetryProxy::DEFAULT_MAX_TRIES,
+            RetryProxy::DEFAULT_BACKOFF_INTERVAL,
+            RetryProxy::DEFAULT_EXCEPTED_EXCEPTIONS,
+            $sqlCodes
+        );
+
+        try {
+            $retryProxy->call(function () {
+                $res = $this->db->query('SELECT SOMETHING FROM NOTHING;');
+            });
+        } catch (\Throwable $e) {
+            echo "\nERROR: " . $e->getMessage() . "\n";
+            echo "\nclass: " . get_class($e) . "\n";
+        }
+    }
+
+    public function ignorableSqlCodesProvider() {
+        return [
+            [['42000']],
+            [['^42']]
+        ];
+    }
+
+    public function retriableSqlCodesProvider() {
+        return [
+            [['HY000']],
+            [['^HY']]
+        ];
+    }
+}
