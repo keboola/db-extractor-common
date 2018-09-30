1579b7ff44edf2ce523f4ac8a3ad66406e5576cd
David Střítežský <stritezskyd@gmail.com>
2018-09-30 10:37:03 +0200

    Clean run.php for Adapter DataDir Tests

--- tests/FunctionalAdapter/run.php
+++ tests/FunctionalAdapter/run.php
@@ -4,7 +4,6 @@ declare(strict_types=1);
 
 require_once __DIR__ . '/../../vendor/autoload.php';
 
-use Keboola\DbExtractor\Application;
 use Keboola\Component\UserException;
 use Keboola\Component\Logger;
 use Keboola\DbExtractor\Extractor\CommonExtractor;
@@ -17,36 +16,11 @@ $logger = new Logger();
 
 
 try {
-    $commonExtractor = new CommonExtractor($logger, $config['parameters']['db']);
-
     // @todo - pass state from state.json
+    $commonExtractor = new CommonExtractor($logger, $config['parameters']['db']);
     $extractorAdapter = new ExtractorAdapter($commonExtractor, $logger, $config['action'], $config['parameters']);
     $extractorAdapter->run();
 
-    exit(0);
-} catch (UserException $e) {
-    $logger->error($e->getMessage());
-    exit(1);
-} catch (\Throwable $e) {
-    $logger->critical(
-        get_class($e) . ':' . $e->getMessage(),
-        [
-            'errFile' => $e->getFile(),
-            'errLine' => $e->getLine(),
-            'errCode' => $e->getCode(),
-            'errTrace' => $e->getTraceAsString(),
-            'errPrevious' => $e->getPrevious() ? get_class($e->getPrevious()) : '',
-        ]
-    );
-    exit(2);
-}
-
-
-exit;
-
-try {
-    $app = new Application($config, $logger);
-    $app->run();
     exit(0);
 } catch (UserException $e) {
     $logger->error($e->getMessage());
