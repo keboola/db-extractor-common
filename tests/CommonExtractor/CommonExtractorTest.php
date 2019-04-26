51a4afeba35abe18ff6b61937e97ebc7af062b71
Tomáš Fejfar <tomas.fejfar@gmail.com>
2019-04-26 15:51:08 +0200

    wip

--- tests/CommonExtractor/CommonExtractorTest.php
+++ tests/CommonExtractor/CommonExtractorTest.php
@@ -6,17 +6,29 @@ namespace Keboola\DbExtractor\Tests\CommonExtractor;
 
 use Keboola\DbExtractor\Test\AbstractExtractorTest;
 use Keboola\DbExtractor\Test\DataLoaderInterface;
+use PDO;
 
 class CommonExtractorTest extends AbstractExtractorTest
 {
     protected function getDataLoader(): DataLoaderInterface
     {
-        return new CommonExtractorDataLoader(
-            $this->getEnv(self::DRIVER, 'DB_HOST'),
-            $this->getEnv(self::DRIVER, 'DB_PORT'),
-            $this->getEnv(self::DRIVER, 'DB_DATABASE'),
-            $this->getEnv(self::DRIVER, 'DB_USER'),
-            $this->getEnv(self::DRIVER, 'DB_PASSWORD')
+        $host = $this->getEnv(self::DRIVER, 'DB_HOST');
+        $port = $this->getEnv(self::DRIVER, 'DB_PORT');
+        $dbname = $this->getEnv(self::DRIVER, 'DB_DATABASE');
+        $user = $this->getEnv(self::DRIVER, 'DB_USER');
+        $pass = $this->getEnv(self::DRIVER, 'DB_PASSWORD');
+
+        $dsn = sprintf("mysql:host=%s;port=%s;dbname=%s;charset=utf8", $host, $port, $dbname);
+
+        $pdo = new PDO(
+            $dsn,
+            $user,
+            $pass,
+            [
+                PDO::MYSQL_ATTR_LOCAL_INFILE => true,
+                PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+            ]
         );
+        return new CommonExtractorDataLoader($pdo);
     }
 }
